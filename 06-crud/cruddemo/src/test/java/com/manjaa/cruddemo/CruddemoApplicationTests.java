4646442f9ad793a2a5c884e1806fd691a3a6d14b
Codemanja <codemanjaa@gmail.com>
2023-04-08 11:34:40 +0300

    Add the first series

---
+++ 06-crud/cruddemo/src/test/java/com/manjaa/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manjaa.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
